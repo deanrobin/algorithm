01b4b11cd2090e188b81ce0b11d9f10e0cc6428f
yuanchen.suo <yuanchen.suo@okcoin.com>
2020-07-06 17:55:04 +0800

    加上fastjson; T1题目

---
+++ src/main/java/leetcode/T1.java
@@ -0,0 +1,63 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+
+public class T1 {
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+     你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+      
+
+     示例:
+
+     给定 nums = [2, 7, 11, 15], target = 9
+
+     因为 nums[0] + nums[1] = 2 + 7 = 9
+     所以返回 [0, 1]
+
+     来源：力扣（LeetCode）
+     链接：https://leetcode-cn.com/problems/two-sum
+     著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i =0; i < nums.length; ++i) {
+            int result = target - nums[i];
+
+            if (map.containsKey(result)) {
+                return new int[] {map.get(result), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        throw new RuntimeException("没有符合的条件");
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        //int[] nums = {1, 3, 5, 9, 10, 12};
+        //int target = 10;
+
+        int[] result = new T1().twoSum(nums, target);
+
+        System.out.println(JSON.toJSON(result));
+
+        // 验证
+        int sum = nums[result[0]] + nums[result[1]];
+        System.out.println(sum == target);
+    }
+
+
+
+
+}
